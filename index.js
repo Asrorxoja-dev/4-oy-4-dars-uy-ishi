5ae08af8eebc1422272f4088f4d6469c16bd40b4
asrorxoja07 <asrorxojaisomiddinov21@gmail.com>
2023-12-14 22:17:12 +0500

    masalalar

---
+++ index.js
@@ -0,0 +1,169 @@
+// 1-masala
+
+// let n = 30;
+
+// let counter = 0;
+
+// for(let i = 1; i <= n; i+3) {
+//     if   ( n % 3 == 0 && n % 7 == 0) {
+//         counter = counter + 1;
+//     }
+// }
+
+
+// console.log(counter);
+
+
+// 2-masala
+
+// let n = 100;
+
+// let sum = 0;
+
+// for (let i = n; i <= 1; i++) {
+//     if (n % 2 ==0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+
+
+
+
+
+// 3-masala
+
+// let n= 10;
+
+// let sum = 0;
+
+// for (let i =1;  i <=n; i++){
+//     if (n % 5 ==0 && n % 7 != 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+
+
+
+
+
+
+
+// 4-masala
+
+
+
+// let a = -5;
+// let b = 1;
+
+// let sum=0;
+
+// for ( i = a; i <=b; i++) {
+//     if (a < 0){
+//    sum += i;
+//     }
+// }
+
+// console.log(sum);
+
+
+
+
+// 5-masala
+
+// let son = 10;
+
+// for (let i=1; i <= son; i++){
+//     son = son * i ;
+// }
+
+// console.log(son);
+
+
+// 6-masala
+
+// let son=10;
+
+// let boluvchilar=1;
+// for (let i = 1; i <= son; i++) {
+//     if (son % i == 0) {
+       
+//    console.log(i);
+
+//     }
+// }
+
+
+
+
+
+// 7-masala
+
+
+// let son=10;
+
+// let counter=0;
+// for (let i = 1; i <= son; i++) {
+//     if (son % i == 0) {
+//        counter++;
+ 
+
+//     }
+// }
+
+// console.log(counter);
+
+
+
+
+
+// 8-masala
+
+// let son=11;
+
+// if (son % 2 == 1) {
+//     console.log("tub son")
+// }else {
+//     console.log("tub son emas")
+
+// }
+
+
+
+
+// 9-masala
+
+
+// let n=5;
+// let sum=1;
+
+// for (let i =1; i <= n; i++){
+
+//     sum += i * i ;
+  
+    
+
+// }
+
+// console.log(sum);
+
+
+
+
+// 10-masala
+
+// let n =10;
+// let sum=0;
+// for(let i=1; i <=n; i++){
+  
+
+// if (n % i == 1) {
+//   sum= i++;
+
+// }
+
+// }
+// console.log(sum);
